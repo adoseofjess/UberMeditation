3f7bd52f071f26e777fc8798ab5982b7d4f4938b
Jessica <jessjkim@gmail.com>
2016-05-12 17:56:42 -0400

    Add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :meditations, only: [:new, :create]
+  resources :static_pages do
+    collection do
+      get "landing"
+      get "home"
+    end
+  end
 
-  root to: "static_pages#home"
+  root to: "static_pages#landing"
 
-  get 'static_pages/home'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
